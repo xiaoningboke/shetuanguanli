d244a5d0c784708d41f6cd4acfb3d50b534dba77
xiaoningboke <sunxiaoning@aliyun.com>
2019-03-23 16:46:10 +0800

    样式以及后台初始化

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+/**
+ * 系统管理员控制器
+ */
+class IndexController extends Controller {
+    public function index(){
+    	$this->display();
+    }
+}
